1f21eba92b1afec833fadd26e7814f2b858bfc06
peter65374 <futuretrader@gmail.com>
2025-01-17 11:31:49 +0800

    Add initial iOS project files and setup for askLunar app

---
+++ askLunar/askLunar/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  askLunar
+//
+//  Created by PeterReturn on 17/1/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ askLunar/askLunar/askLunarApp.swift
@@ -0,0 +1,32 @@
+//
+//  askLunarApp.swift
+//  askLunar
+//
+//  Created by PeterReturn on 17/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct askLunarApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
